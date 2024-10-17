d51da0ec51538762d5b5d9be061f964a8837e8cf
Montana Nicholson <nicholsonmontana@gmail.com>
2024-10-17 02:07:04 -0500

    Project initialization and safety method

---
+++ BankersAlgorithm/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace BankersAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] available = [3, 3, 2];
+            int[][] max = [[7, 5, 3],
+                           [3, 2, 2],
+                           [9, 0, 2],
+                           [2, 2, 2],
+                           [4, 3, 3]];
+            int[][] allocation = [[0, 1, 0],
+                                  [2, 0, 0],
+                                  [3, 0, 2],
+                                  [2, 1, 1],
+                                  [0, 0, 2]];
+        }
+        static bool request(int[] available)
+        {
+
+        }
+        static bool Safety(int[] work, int[] need)
+        {
+            bool[] finish = new bool[work.Length];
+
+            for (int i = 0; i < finish.Length; i++)
+            {
+                finish[i] = false;
+            }
+            for (int i = 0; i <= work.Length; i++)
+            {
+                if (!finish[i] && need[i] <= work[i])
+                {
+                    finish[i] = true;
+                }
+            }
+            for (int i = 0; i < finish.Length; i++)
+            {
+                if (!finish[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
